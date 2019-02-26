425264a2f75f14f1bf9511acf20af7e3fca50e44
Alireza Bashiri <azbshiri@gmail.com>
2019-02-26 21:27:29 +0700

    convert rspec asserations to minitest

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -40,227 +40,255 @@ class GildedRoseTest < MiniTest::Test
     assert_equal 4, item.days_remaining
   end
 
-  # def test_brie_before_sell_date
-  #   gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: 5, quality: 10)
+  def test_brie_before_sell_date
+    gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: 5, quality: 10)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: 4, quality: 11)
-  # end
+    assert_equal gilded_rose.days_remaining, 4
+    assert_equal gilded_rose.quality, 11
+  end
 
-  # def test_brie_with_max_quality
-  #   gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: 5, quality: 50)
+  def test_brie_with_max_quality
+    gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: 5, quality: 50)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: 4, quality: 50)
-  # end
+    assert_equal gilded_rose.days_remaining, 4
+    assert_equal gilded_rose.quality, 50
+  end
 
-  # def test_brie_on_sell_date
-  #   gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: 0, quality: 10)
+  def test_brie_on_sell_date
+    gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: 0, quality: 10)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: -1, quality: 12)
-  # end
+    assert_equal gilded_rose.days_remaining, -1
+    assert_equal gilded_rose.quality, 12
+  end
 
-  # def test_brie_on_sell_date_near_max_quality
-  #   gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: 0, quality: 49)
+  def test_brie_on_sell_date_near_max_quality
+    gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: 0, quality: 49)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: -1, quality: 50)
-  # end
+    assert_equal gilded_rose.days_remaining, -1
+    assert_equal gilded_rose.quality, 50
+  end
 
-  # def test_brie_on_sell_date_with_max_quality
-  #   gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: 0, quality: 50)
+  def test_brie_on_sell_date_with_max_quality
+    gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: 0, quality: 50)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: -1, quality: 50)
-  # end
+    assert_equal gilded_rose.days_remaining, -1
+    assert_equal gilded_rose.quality, 50
+  end
 
-  # def test_brie_after_sell_date
-  #   gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: -10, quality: 10)
+  def test_brie_after_sell_date
+    gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: -10, quality: 10)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: -11, quality: 12)
-  # end
+    assert_equal gilded_rose.days_remaining, -11
+    assert_equal gilded_rose.quality, 12
+  end
 
-  # def test_brie_after_sell_date_with_max_quality
-  #   gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: -10, quality: 50)
+  def test_brie_after_sell_date_with_max_quality
+    gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: -10, quality: 50)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: -11, quality: 50)
-  # end
+    assert_equal gilded_rose.days_remaining, -11
+    assert_equal gilded_rose.quality, 50
+  end
 
-  # def test_sulfuras_before_sell_date
-  #   gilded_rose = GildedRose.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: 5, quality: 80)
+  def test_sulfuras_before_sell_date
+    gilded_rose = GildedRose.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: 5, quality: 80)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: 5, quality: 80)
-  # end
+    assert_equal gilded_rose.days_remaining, 5
+    assert_equal gilded_rose.quality, 80
+  end
 
-  # def test_sulfuras_on_sell_date
-  #   gilded_rose = GildedRose.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: 0, quality: 80)
+  def test_sulfuras_on_sell_date
+    gilded_rose = GildedRose.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: 0, quality: 80)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: 0, quality: 80)
-  # end
+    assert_equal gilded_rose.days_remaining, 0
+    assert_equal gilded_rose.quality, 80
+  end
 
-  # def test_sulfuras_after_sell_date
-  #   gilded_rose = GildedRose.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: -10, quality: 80)
+  def test_sulfuras_after_sell_date
+    gilded_rose = GildedRose.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: -10, quality: 80)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: -10, quality: 80)
-  # end
+    assert_equal gilded_rose.days_remaining, -10
+assert_equal gilded_rose.quality, 80
+  end
 
-  # def test_backstage_long_before_sell_date
-  #   gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 11, quality: 10)
+  def test_backstage_long_before_sell_date
+    gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 11, quality: 10)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: 10, quality: 11)
-  # end
+    assert_equal gilded_rose.days_remaining, 10
+assert_equal gilded_rose.quality, 11
+  end
 
-  # def test_backstage_long_before_sell_date_at_max_quality
-  #   gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 11, quality: 50)
+  def test_backstage_long_before_sell_date_at_max_quality
+    gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 11, quality: 50)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: 10, quality: 50)
-  # end
+    assert_equal gilded_rose.days_remaining, 10
+    assert_equal gilded_rose.quality, 50
+  end
 
-  # def test_backstage_medium_close_to_sell_date_upper_bound
-  #   gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 10, quality: 10)
+  def test_backstage_medium_close_to_sell_date_upper_bound
+    gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 10, quality: 10)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: 9, quality: 12)
-  # end
+    assert_equal gilded_rose.days_remaining, 9
+    assert_equal gilded_rose.quality, 12
+  end
 
-  # def test_backstage_medium_close_to_sell_date_upper_bound_at_max_quality
-  #   gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 10, quality: 50)
+  def test_backstage_medium_close_to_sell_date_upper_bound_at_max_quality
+    gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 10, quality: 50)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: 9, quality: 50)
-  # end
+    assert_equal gilded_rose.days_remaining, 9
+    assert_equal gilded_rose.quality, 50
+  end
 
-  # def test_backstage_medium_close_to_sell_date_lower_bound
-  #   gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 6, quality: 10)
+  def test_backstage_medium_close_to_sell_date_lower_bound
+    gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 6, quality: 10)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: 5, quality: 12)
-  # end
+    assert_equal gilded_rose.days_remaining, 5
+    assert_equal gilded_rose.quality, 12
+  end
 
-  # def test_backstage_medium_close_to_sell_date_lower_bound_at_max_quality
-  #   gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 6, quality: 50)
+  def test_backstage_medium_close_to_sell_date_lower_bound_at_max_quality
+    gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 6, quality: 50)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: 5, quality: 50)
-  # end
+    assert_equal gilded_rose.days_remaining, 5
+    assert_equal gilded_rose.quality, 50
+  end
 
-  # def test_backstage_very_close_to_sell_date_upper_bound
-  #   gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 5, quality: 10)
+  def test_backstage_very_close_to_sell_date_upper_bound
+    gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 5, quality: 10)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: 4, quality: 13)
-  # end
+    assert_equal gilded_rose.days_remaining, 4
+    assert_equal gilded_rose.quality, 13
+  end
 
-  # def test_backstage_very_close_to_sell_date_upper_bound_at_max_quality
-  #   gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 5, quality: 50)
+  def test_backstage_very_close_to_sell_date_upper_bound_at_max_quality
+    gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 5, quality: 50)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: 4, quality: 50)
-  # end
+    assert_equal gilded_rose.days_remaining, 4
+    assert_equal gilded_rose.quality, 50
+  end
 
-  # def test_backstage_very_close_to_sell_date_lower_bound
-  #   gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 1, quality: 10)
+  def test_backstage_very_close_to_sell_date_lower_bound
+    gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 1, quality: 10)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: 0, quality: 13)
-  # end
+    assert_equal gilded_rose.days_remaining, 0
+    assert_equal gilded_rose.quality, 13
+  end
 
-  # def test_backstage_very_close_to_sell_date_lower_bound_at_max_quality
-  #   gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 1, quality: 50)
+  def test_backstage_very_close_to_sell_date_lower_bound_at_max_quality
+    gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 1, quality: 50)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: 0, quality: 50)
-  # end
+    assert_equal gilded_rose.days_remaining, 0
+    assert_equal gilded_rose.quality, 50
+  end
 
-  # def test_backstage_on_sell_date
-  #   gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 0, quality: 10)
+  def test_backstage_on_sell_date
+    gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 0, quality: 10)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: -1, quality: 0)
-  # end
+    assert_equal gilded_rose.days_remaining, -1
+    assert_equal gilded_rose.quality, 0
+  end
 
-  # def test_backstage_after_sell_date
-  #   gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: -10, quality: 10)
+  def test_backstage_after_sell_date
+    gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: -10, quality: 10)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: -11, quality: 0)
-  # end
+    assert_equal gilded_rose.days_remaining, -11
+    assert_equal gilded_rose.quality, 0
+  end
 
-  # def test_conjured_before_sell_date
-  #   gilded_rose = GildedRose.new(name: "Conjured Mana Cake", days_remaining: 5, quality: 10)
+  def test_conjured_before_sell_date
+    gilded_rose = GildedRose.new(name: "Conjured Mana Cake", days_remaining: 5, quality: 10)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: 4, quality: 8)
-  # end
+    assert_equal gilded_rose.days_remaining, 4
+    assert_equal gilded_rose.quality, 8
+  end
 
-  # def test_conjured_before_sell_date_at_zero_quality
-  #   gilded_rose = GildedRose.new(name: "Conjured Mana Cake", days_remaining: 5, quality: 0)
+  def test_conjured_before_sell_date_at_zero_quality
+    gilded_rose = GildedRose.new(name: "Conjured Mana Cake", days_remaining: 5, quality: 0)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: 4, quality: 0)
-  # end
+    assert_equal gilded_rose.days_remaining, 4
+    assert_equal gilded_rose.quality, 0
+  end
 
-  # def test_conjured_on_sell_date
-  #   gilded_rose = GildedRose.new(name: "Conjured Mana Cake", days_remaining: 0, quality: 10)
+  def test_conjured_on_sell_date
+    gilded_rose = GildedRose.new(name: "Conjured Mana Cake", days_remaining: 0, quality: 10)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: -1, quality: 6)
-  # end
+    assert_equal gilded_rose.days_remaining, -1
+    assert_equal gilded_rose.quality, 6
+  end
 
-  # def test_conjured_on_sell_date_at_zero_quality
-  #   gilded_rose = GildedRose.new(name: "Conjured Mana Cake", days_remaining: 0, quality: 0)
+  def test_conjured_on_sell_date_at_zero_quality
+    gilded_rose = GildedRose.new(name: "Conjured Mana Cake", days_remaining: 0, quality: 0)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: -1, quality: 0)
-  # end
+    assert_equal gilded_rose.days_remaining, -1
+    assert_equal gilded_rose.quality, 0
+  end
 
-  # def test_conjured_after_sell_date
-  #   gilded_rose = GildedRose.new(name: "Conjured Mana Cake", days_remaining: -10, quality: 10)
+  def test_conjured_after_sell_date
+    gilded_rose = GildedRose.new(name: "Conjured Mana Cake", days_remaining: -10, quality: 10)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: -11, quality: 6)
-  # end
+    assert_equal gilded_rose.days_remaining, -11
+    assert_equal gilded_rose.quality, 6
+  end
 
-  # def test_conjured_after_sell_date_at_zero_quality
-  #   gilded_rose = GildedRose.new(name: "Conjured Mana Cake", days_remaining: -10, quality: 0)
+  def test_conjured_after_sell_date_at_zero_quality
+    gilded_rose = GildedRose.new(name: "Conjured Mana Cake", days_remaining: -10, quality: 0)
 
-  #   gilded_rose.tick
+    gilded_rose.tick
 
-  #   expect(gilded_rose).to have_attributes(days_remaining: -11, quality: 0)
-  # end
+    assert_equal gilded_rose.days_remaining, -11
+    assert_equal gilded_rose.quality, 0
+  end
 end
